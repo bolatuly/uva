09160bc707f4b4b507825e5cd6ace1bde1d159dd
bolatuly <b.a.bolatuly@gmail.com>
2018-03-07 21:54:46 +0900

    10264 part. solved

---
+++ 10264.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+//NOT SOLVED YET
+int main() {
+
+    int dim;
+
+    while (cin >> dim) {
+
+        int edges = pow(2, dim);
+        int edges_array[edges];
+
+        for (int i = 0; i < 1 << dim; i++) {
+            cin >> edges_array[i];
+        }
+
+        int sums[edges];
+        for (int i = 0; i < 1 << dim; i++) {
+            int sum = 0;
+            for (int j = 0; j < dim; j++) {
+                int b = i ^(1 << j);
+                sum += edges_array[b] + edges_array[i];
+            }
+            sums[i] = sum;
+        }
+        int max = 0;
+        for (int i = 0; i < 1 << dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                if (sums[i] + sums[i ^ (1 << j)] > max) {
+                    max = sums[i] + sums[i ^ (1 << j)];
+                }
+            }
+        }
+        cout << max << endl;
+    }
+    return 0;
+}
--- 12398.cpp
+++ 12398.cpp
@@ -4,20 +4,20 @@
 using namespace std;
 
 void increment(int a[3][3], int x, int y) {
+
+    a[x][y] += 1;
+
     if (a[x][y] > 9) {
-        a[x][y] -= 10;
-    } else {
-        a[x][y] += 1;
+        a[x][y] = 0;
     }
 }
 
 void out(int n, int a[3][3]) {
-    cout << "Case #" << (n) + 1 << ":" << endl;
+    cout << "Case #" << n << ":" << endl;
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            cout << a[i][j] << " ";
+            printf("%d%c", a[i][j] % 10, j == 2 ? 10 : 32);
         }
-        cout << endl;
     }
 }
 
@@ -30,9 +30,11 @@ int main() {
 
     while (getline(cin, path)) {
 
+        n++;
+
         if (path.empty()) {
             out(n, data);
-            break;
+            continue;
         }
 
         for (int k = 0; k < path.length(); k++) {
@@ -50,7 +52,6 @@ int main() {
 
         out(n, data);
 
-        n++;
         memset(data, 0, sizeof(data));
     }
 
